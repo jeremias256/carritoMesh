a87b5088bf3d682b7b0556c94bc9435bfbaf51f1
jeremias256 <jere.menacho@gmail.com>
2023-11-22 13:27:01 -0300

    fix mesh

--- src/steps/Agendamiento.jsx
+++ src/steps/Agendamiento.jsx
@@ -1,6 +1,16 @@
+import CalendarioJS from "./CalendarioJS";
 
 export const Agendamiento = () => {
   return (
-    <div>Agendamiento</div>
-  )
-}
+    <>
+      <CalendarioJS />
+      <iframe
+        height="550px"
+        src="https://portal2-des.iplan.com.ar/agendamientoBiz/calendar/?site=1682007543&amp;tur=2&amp;tipo=IN"
+        style="border:none;width:100%;"
+        title="Agendamiento"
+        width="600px"
+      ></iframe>
+    </>
+  );
+};
